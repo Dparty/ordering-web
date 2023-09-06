9c1a90230aad5a5ef65ee0664640f6d332cdb924
liuyaping-99 <494433601@qq.com>
2023-09-06 21:57:09 +0800

    fix

--- src/views/order/components/Menu/index.tsx
+++ src/views/order/components/Menu/index.tsx
@@ -1,65 +1,90 @@
-import "./index.css";
-import { FoodProps, Order } from "../../index";
-import { useMemo, useState } from "react";
-import React from "react";
-import FoodCard from "../FoodCard";
-import { Item } from "warmsilver-core-ts-sdk";
-import { getAmount } from "../../../../utils";
+import './index.css'
+import { Order } from '../../index'
+import { useEffect, useMemo, useState } from 'react'
+import React from 'react'
+import FoodCard from '../FoodCard'
+import { Item } from 'warmsilver-core-ts-sdk'
+import { getAmount } from '../../../../utils'
 
 interface IProps {
-  pushCart: (item: Item, selectedOptions: Map<string, string>) => void;
-  items: Item[];
-  orders: Order[];
-  removeCart: (item: Item, selectedOptions: Map<string, string>) => void;
-  onSelect: (item: Item) => void;
+  pushCart: (item: Item, selectedOptions: Map<string, string>) => void
+  items: Item[]
+  orders: Order[]
+  removeCart: (item: Item, selectedOptions: Map<string, string>) => void
+  onSelect: (item: Item) => void
 }
 
-const Menu: React.FC<IProps> = ({
-  removeCart,
-  onSelect,
-  items,
-  orders,
-  pushCart,
-}) => {
-  // const [activeMenuId, setActiveMenuId] = useState<string>(menus[0].id);
-  const [isScrollingTo, setScrollingTo] = useState<string | null>(null);
-
-  const clickLeftMenu = (id: string) => {
-    setScrollingTo(id);
-    // menuRefs[id].current!.scrollIntoView({ behavior: "smooth" });
-    setTimeout(() => {
-      // setActiveMenuId(id);
-      setScrollingTo(null); // 点击事件的滚动结束，重置isScrollingTo
-    }, 500); // 延时应稍大于滚动动画的时间
-  };
+const Menu: React.FC<IProps> = ({ removeCart, onSelect, items, orders, pushCart }) => {
   const getItemsByTag = (tag: string) => {
-    return items.filter(
-      (item) => item.tags.filter((t) => t === tag).length !== 0
-    );
-  };
+    return items.filter(item => item.tags.filter(t => t === tag).length !== 0)
+  }
   const tags = useMemo(() => {
-    const tags = new Set<string>();
+    const tags = new Set<string>()
     items
       .reduce((tags, item) => {
-        return [...tags, ...item.tags];
+        return [...tags, ...item.tags]
       }, new Array<string>())
-      .forEach((tag) => tags.add(tag));
-    return Array.from(tags);
-  }, [items]);
+      .forEach(tag => tags.add(tag))
+    return Array.from(tags)
+  }, [items])
+
+  const [activeTag, setActiveTag] = useState<string>(tags[0])
+  const [isScrollingTo, setScrollingTo] = useState<string | null>(null)
+
+  const tagRefs: { [key: string]: React.RefObject<HTMLDivElement> } = tags.reduce(
+    (refs: { [key: string]: React.RefObject<HTMLDivElement> }, tag) => {
+      refs[tag] = React.createRef<HTMLDivElement>()
+      return refs
+    },
+    {}
+  )
+  //点击左侧菜单时
+  const clickLeftTag = (clickTag: string) => {
+    setScrollingTo(clickTag)
+    tagRefs[clickTag].current!.scrollIntoView({ behavior: 'smooth' })
+    setTimeout(() => {
+      setActiveTag(clickTag)
+      setScrollingTo(null) // 点击事件的滚动结束，重置isScrollingTo
+    }, 500) // 延时应稍大于滚动动画的时间
+  }
+
+  //右侧菜单滚动
+  const handleScroll = (event: React.UIEvent<HTMLDivElement>): void => {
+    if (isScrollingTo) return
+    if (!event.nativeEvent.isTrusted) return
+
+    const _activeTag = tags.find(tag => {
+      const rect = tagRefs[tag].current!.getBoundingClientRect()
+      return rect.top < 280 && rect.bottom > 280
+    })
+
+    setActiveTag(_activeTag ? _activeTag : activeTag)
+  }
+
+  useEffect(() => {
+    if (tags.length && !activeTag) {
+      setActiveTag(tags[0])
+    }
+  }, [tags])
+
   return (
     <div className="menu page-container">
       <div className="left-menu">
         {tags.map((tag, i) => (
           <div
             key={i}
-            className={`left-menu-item ${true ? "active" : "normal"}`}>
+            className={`left-menu-item ${tag === activeTag ? 'active' : 'normal'}`}
+            onClick={() => {
+              clickLeftTag(tag)
+            }}
+          >
             <span>{tag}</span>
           </div>
         ))}
       </div>
-      <div className="right-menu">
+      <div className="right-menu" onScroll={handleScroll}>
         {tags.map((tag, index) => (
-          <div key={index}>
+          <div key={index} ref={tagRefs[tag]}>
             <div className="right-menu-type text-ellipsis">{tag}</div>
             {getItemsByTag(tag).map((item, index) => (
               <FoodCard
@@ -75,7 +100,7 @@ const Menu: React.FC<IProps> = ({
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Menu;
+export default Menu
