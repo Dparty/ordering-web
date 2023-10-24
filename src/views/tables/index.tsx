64f5391b6c8b8415963802a78da8f858a10bd4a8
Dan Chen <dan.chen@boardware.com>
2023-10-24 18:57:26 +0800

    feat

--- src/views/tables/index.tsx
+++ src/views/tables/index.tsx
@@ -4,20 +4,36 @@ import { useEffect, useState } from "react";
 import { restaurantApi } from "../../api/api";
 import { Table } from "@dparty/core-ts-sdk";
 import SelectTableNum from "./SelectTableNum";
-
+import QRCode from "react-qr-code";
 interface ISelectTableModalProps {
   restaurantId: string;
   tableList: Table[] | null;
   title?: string;
 }
 
+const qrcodeApi = "https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=";
+const orderWebDomain = "https://ordering-uat.sum-foods.com";
+
+function createQrcodeUrl(text: string): string {
+  return `${qrcodeApi}${text}`;
+}
+
+function createOrderingUrl(restaurantId: string, tableId: string): string {
+  return createQrcodeUrl(
+    encodeURIComponent(
+      `${orderWebDomain}/ordering/?restaurantId=${restaurantId}&tableId=${tableId}`
+    )
+  );
+}
+
 const SelectTableModal: React.FC<ISelectTableModalProps> = ({ restaurantId, tableList, title }) => {
   const [selectedOption, setSelectedOption] = useState<Table | null>(null);
-
+  const [showQrcode, setShowQrcode] = useState(false);
   const onSelectOption = (option: Table) => {
     setSelectedOption(option);
   };
-
+  // console.log(createOrderingUrl(restaurantId,selectedOption.id));
+  // setShowQrcode(true);
   return (
     <div className="select-tables">
       <div className="select-tables__mask"></div>
@@ -41,10 +57,33 @@ const SelectTableModal: React.FC<ISelectTableModalProps> = ({ restaurantId, tabl
               >
                 開始點餐
               </button>
+              <button
+                style={{ marginLeft: 10 }}
+                className={"select-tables-submit-btn select-tables-submit-btn-usable"}
+                onClick={() => {
+                  setShowQrcode(true);
+                }}
+              >
+                展示Qrcode
+              </button>
             </div>
           </div>
         </div>
       </div>
+      {showQrcode && (
+        <div onClick={() => setShowQrcode(false)} id="myModal" className="modal">
+          <div className="modal-content">
+            {selectedOption && (
+              <QRCode
+                size={256}
+                style={{ height: "auto", maxWidth: "50%", width: "50%" }}
+                value={createQrcodeUrl(selectedOption.id)}
+                viewBox={`0 0 256 256`}
+              />
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
@@ -78,7 +117,7 @@ const TablesPage = () => {
       <SelectTableModal
         restaurantId={restaurantId}
         tableList={tableList}
-        title={"桌號獲取失敗，請手動選擇桌號"}
+        // title={"桌號獲取失敗，請手動選擇桌號"}
       />
       <img className="tables__logo" src={logoPngUrl} alt="logo" />
     </div>
